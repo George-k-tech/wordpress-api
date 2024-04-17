40d7e125a899810ce51caeb5c4cb36f0ce397f11
George <kariukigeorge511@gmail.com>
2024-04-17 18:12:09 +0300

    migrations

---
+++ database/migrations/2024_04_17_145411_create_customer_details_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customer_details', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->autoIncrement();
+            $table->string('first_name')->nullable();
+            $table->string('last-name')->nullable();
+            $table->string('company_name')->nullable();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->boolean('create_account')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customer_details');
+    }
+};
---
+++ database/migrations/2024_04_17_145442_create_payment_details_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payment_details', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->autoIncrement();
+            $table->unsignedBigInteger('customer_id')->nullable();
+            $table->string('payment_method')->nullable();
+            $table->string('paypal_account')->nullable();
+            $table->string('bank_account')->nullable();
+            $table->string('cheque_details')->nullable();
+            $table->timestamps();
+
+            $table->foreign('customer_id')->references('id')->on('customer_details')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payment_details');
+    }
+};
